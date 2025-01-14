c004daca4f0d93da59b20d09bb54c5d07ad7fe20
eivindholvik <eivind.holvik@noroff.no>
2025-01-14 11:25:47 +0100

    lecture done

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,8 @@
 var typed = new Typed(".aboutMe", {
-    strings: ["John Doe", "The Cake Master","a Confectioner","a Man with Passion"],
+    strings: ["John Doe", "The Cake Master", "a Confectioner", "a Man with Passion"],
     smartBackspace: true,
     typeSpeed: 100,
-    backSpeed: 100,
+    backSpeed: 50,
     loop: true,
     loopCount: Infinity,
     startDelay: 1000
--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const fs = require("fs");
+const path = require("path");
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('home');
+  const data = fs.readFileSync(path.resolve(__dirname, "../data/introductionArray.json"));
+  res.render('home', { introArray: JSON.parse(data) });
 });
+
+router.post("/", (req, res, next) => {
+  const dataFromFile = fs.readFileSync(path.resolve(__dirname, "../data/introductionArray.json"));
+  const arrayFromFile = JSON.parse(dataFromFile);
+  const newLine = req.body.newText;
+  let result = arrayFromFile.findIndex((text) => text === newLine);
+  if (result !== -1) {
+    res.end("Duplicate found");
+    return;
+  }
+  arrayFromFile.push(newLine);
+  fs.writeFileSync(path.resolve(__dirname, "../data/introductionArray.json"), JSON.stringify(arrayFromFile));
+  res.end("New line added");
+})
 
 
 
