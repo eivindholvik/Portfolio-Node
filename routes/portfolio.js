bfc4e067e759f52e2bd944d72c20ef69b87aa4be
eivindholvik <eivind.holvik@noroff.no>
2025-01-15 11:31:49 +0100

    Add lecture

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -1,11 +1,56 @@
 const express = require('express');
 const router = express.Router();
+const fs = require("fs");
+const path = require("path");
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
+const request = require("request");
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('portfolio');
+  const dataFromFile = fs.readFileSync(path.resolve(__dirname, "../data/portfolio.json"));
+  const cakes = JSON.parse(dataFromFile);
+  res.render('portfolio', { cakes });
 });
 
+router.post("/", jsonParser, (req, res, next) => {
+  const dataFromFile = fs.readFileSync(path.resolve(__dirname, "../data/portfolio.json"));
+  const cakes = JSON.parse(dataFromFile);
+  const filtered = cakes.filter(({ name }) => name === req.body.name);
+  if (filtered.length !== 0) {
+    res.end("The name of the cake already exsits");
+    return;
+  }
+  download(req.body.url, req.body.name, () => {
+    console.log(`${req.body.name} was successfully downloaded to path`);
+  });
+  cakes.push(req.body);
+  fs.writeFileSync(path.resolve(__dirname, "../data/portfolio.json"), JSON.stringify(cakes));
+  res.end(`New cake added to portfolio`);
 
+});
+
+router.delete("/", jsonParser, (req, res, next) => {
+  const dataFromFile = fs.readFileSync(path.resolve(__dirname, "../data/portfolio.json"));
+  const cakes = JSON.parse(dataFromFile);
+  const filtered = cakes.filter(({ name }) => name !== req.body.name);
+  if (filtered.length === cakes.length) {
+    res.end("Could not find the name");
+    return;
+  }
+  fs.unlink(path.resolve(__dirname, "../data/img/" + req.body.name), () => {
+    console.log(`${req.body.name} was successfully delete!`);
+  });
+  fs.writeFileSync(path.resolve(__dirname, "../data/portfolio.json"), JSON.stringify(filtered));
+  res.end("Successfully deleted cake");
+});
+
+//download image to the server:
+function download(url, filename, callback) {
+  request.head(url, function (err, res, body) {
+    request(url).pipe(fs.createWriteStream(path.resolve(__dirname, '../data/img/' + filename))).on('close', callback);
+  });
+};
 
 module.exports = router;
--- routes/recommendations.js
+++ routes/recommendations.js
@@ -15,7 +15,6 @@ router.post("/", jsonParser, (req, res, next) => {
   const dataFromFile = fs.readFileSync(path.resolve(__dirname, "../data/recommendations.json"));
   const recommendationsArr = JSON.parse(dataFromFile);
   const filtered = recommendationsArr.filter(recommendation => recommendation.name === req.body.name);
-  console.log(filtered);
   if (filtered.length === 0) {
     recommendationsArr.push(req.body);
     fs.writeFileSync(path.resolve(__dirname, "../data/recommendations.json"), JSON.stringify(recommendationsArr));
@@ -23,6 +22,18 @@ router.post("/", jsonParser, (req, res, next) => {
     return;
   }
   res.end("User has already made a recommendation")
+});
+
+router.delete("/", jsonParser, (req, res, next) => {
+  const dataFromFile = fs.readFileSync(path.resolve(__dirname, "../data/recommendations.json"));
+  const recommendationsArr = JSON.parse(dataFromFile);
+  const filtered = recommendationsArr.filter(recommendation => recommendation.name !== req.body.name);
+  if (filtered.length !== recommendationsArr) {
+    fs.writeFileSync(path.resolve(__dirname, "../data/recommendations.json"), JSON.stringify(filtered));
+    res.end(`${req.body.name}'s recommendation deleted`);
+    return;
+  }
+  res.end(`Found no recommendation for ${req.body.name}`);
 })
 
 
